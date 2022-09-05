ce5b80da95fe818a1e9748edc7958160a2527dc2
Daniel Zheng <daniel.zheng@stonybrook.edu>
2022-09-05 17:10:59 -0400

    introduced remembering user input -daniel

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 'use strict';
 
+
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.content a');
+
+window.onload = function () {
+  if (localStorage.getItem('popState') != "hide") {
+    setTimeout(function () {
+      popup.style.display = "block";
+      localStorage.setItem('popState', "hide");
+    }, 2000)
+  }
+}
+
+close.addEventListener('click', () => {
+  popup.style.display = "none";
+  localStorage.setItem('popState', "hide");
+})
+
