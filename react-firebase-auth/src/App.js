b25df5e9b5e5098e5495f6ddd73391ecaf83d2c6
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2022-09-06 14:27:17 -0400

    Tested user in database working

--- react-firebase-auth/src/App.js
+++ react-firebase-auth/src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
-import {useState , useEffect} from "react";
+import {useState , useEffect, useSyncExternalStore} from "react";
 import {db} from "./firebase-config";
-import {collection, getDoc} from "firebase/firestore";
+import {collection, getDocs} from "firebase/firestore";
 
 function App() {
-  const [User, setUsers] = useState([]);
+  const [Users, setUsers] = useState([]);
   const userCollectionRef = collection(db, "Users");
   useEffect(() => {
     const getUsers = async () => {
-      const data = await getDoc(userCollectionRef)
-      console.log(data)
+      const data = await getDocs(userCollectionRef)
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
     }
-
     getUsers()
   },[])
+
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <div className="app"> 
+    {Users.map((Users) => {
+      return <div> 
+        <h1> Name : {Users.Username} </h1>
+        <h1> Rank : {Users.Rank}</h1>
+      </div>
+    })}   
+  </div>
+  )
 } 
 
 export default App;
