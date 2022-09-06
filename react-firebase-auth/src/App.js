bf8e10733f3ca14b997d8ef577c8b415ca2e4dc3
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2022-09-06 15:13:48 -0400

    Added a js webpage

--- react-firebase-auth/src/App.js
+++ react-firebase-auth/src/App.js
@@ -15,7 +15,74 @@ function App() {
   },[])
 
   return (
-    <div className="app"> 
+      <div>  
+            <div id="Navbar">
+          <a href="">
+            <button id="Home">
+              <h2>
+                One Move Chess
+              </h2>
+            </button>
+          </a>
+          <ul id="List">
+            <li class="items">
+              <a href="#">
+                <button>
+                  <span class="List-item">Rank</span>
+                </button>
+              </a>
+            </li>
+            <li class="items">
+              <a href="#">
+                <button>
+                  <span class="List-item">Rules</span>
+                </button>
+              </a>
+            </li>
+            <li class="items">
+              <a href="#">
+                <button>
+                  <span class="List-item">Learn More</span>
+                </button>
+              </a>
+            </li>
+            <li class="item">
+              <a href="UserLogin.html">
+                <button>
+                  <span class="List-item">Login</span>
+                </button>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="Login-box-container">
+        <div id="Login-box">
+          <div id="form-login">
+            <span>
+              Login Information
+            </span> 
+            <form type="box" action="#">
+              <div class="Login-infos">
+                  <input type="text" placeholder="Enter Username Or Email..." required/>
+              </div>
+              <div class="Login-infos">
+                  <input type="password" placeholder="Enter Password" required/>
+              </div>
+              <div class="Login-infos">
+                  <input id="submit" type="submit" value="Login"/>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      </div>
+  )
+
+} 
+
+
+/*
+ <div className="app"> 
     {Users.map((Users) => {
       return <div> 
         <h1> Name : {Users.Username} </h1>
@@ -23,7 +90,7 @@ function App() {
       </div>
     })}   
   </div>
-  )
-} 
 
+
+*/
 export default App;
